fcddacb70d9949de4337b2f19b3aa311a5e99b73
haoleir <haoleir@gmail.com>
2021-10-30 15:31:03 +0800

    add:js设计模式-综合案例-购物车

---
+++ javaScript设计模式和开发实践/shoppingCart/src/App.js
@@ -0,0 +1,5 @@
+class App {
+  init() {}
+}
+
+export default App;
---
+++ javaScript设计模式和开发实践/shoppingCart/webpack.dev.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js' // release 会自动创建
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html' // bundle.js 会自动注入
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, './dist'), // 根目录
+    open: true, // 自动打开浏览器
+    port: 9000, // 端口
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:8880'
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
